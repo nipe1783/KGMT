d9b3ad64aa25321de6eaa8b6983fbf4f2e828cda
Nicolas Perrault <nipe1783@colorado.edu>
2024-07-12 10:41:37 -0400

    init commit

---
+++ include/collisionCheck/collisionCheck.cuh
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdio.h>
+
+__device__
+bool isBroadPhaseValid(float 
+
+
+
+
+
+
+*bbMin, float *bbMax, float *obs);
+
+
+
+
+__device__
+bool isMotionValid(float* x0, float* x1, float *bbMin, float *bbMax, float* obstacles, int obstaclesCount);
